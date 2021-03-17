d242349e1c3efd189bf4fe1df0b1544332746fac
sandeeplayam <sandeeplayam@cmail.carleton.ca>
2021-03-17 19:34:42 -0400

    Added ending parking session mutation

--- src/graphql/resolvers/parkingSession.js
+++ src/graphql/resolvers/parkingSession.js
@@ -32,5 +32,51 @@ export default {
         id: result._id,
       };
     },
+    endParkingSession: async (
+      _,
+      { endSession: { id, exitTime, exitDate, elapsedTime } },
+      { ParkingSession }
+    ) => {
+      let parkingSession = await ParkingSession.findOne({ _id: id });
+
+      if (!parkingSession) {
+        let msg = 'parkingSession not found.';
+        throw new UserInputError(msg, {
+          errors: {
+            id: 'provided session id not Found.',
+          },
+        });
+      }
+
+      if (exitTime) {
+        parkingSession.exitTime = exitTime;
+      }
+
+      if (exitDate) {
+        parkingSession.exitDate = exitDate;
+      }
+
+      parkingSession.payAmount = calculatePayment(elapsedTime);
+
+      await parkingSession.save();
+      return {
+        ...result._doc,
+        id: result._id,
+      };
+    },
   },
+};
+
+const calculatePayment = (time) => {
+  let splitTime = time.split(':');
+  let hours = splitTime[0];
+  let minutes = splitTime[1];
+  let seconds = splitTime[2];
+
+  if (parseInt(minutes) > 0 && parseInt(seconds) > 0) {
+    hours = parseInt(hours) + 1;
+  }
+
+  let amount = hours * 2;
+  return '$' + amount.toString();
 };
